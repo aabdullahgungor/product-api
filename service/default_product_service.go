d181be25694dcccd1ac70eeb27d3a213b331aab3
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-03-27 03:53:44 +0300

    product service file added

---
+++ service/default_product_service.go
@@ -0,0 +1,91 @@
+package service
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/aabdullahgungor/product-api/models"
+	"github.com/aabdullahgungor/product-api/repository"
+)
+
+var (
+	ErrIDIsNotValid    = errors.New("id is not valid")
+	ErrNameIsNotEmpty = errors.New("Product title cannot be empty")
+)
+
+type DefaultProductService struct {
+	productRepo repository.IProductRepository
+}
+
+func NewDefaultProductService(pRepo repository.IProductRepository) *DefaultProductService {
+	return &DefaultProductService{
+		productRepo: pRepo,
+	}
+}
+
+
+func (d *DefaultProductService) GetAll() ([]models.Product, error) {
+
+	return d.productRepo.GetAllProducts()
+	
+}
+
+func (d *DefaultProductService) GetById(id string) (models.Product, error) {
+	int_id, _ := strconv.Atoi(id)
+	if int_id <= 0 {
+		return models.Product{}, ErrIDIsNotValid
+	}
+
+	product, err := d.productRepo.GetProductById(id)
+
+	if err != nil {
+		return models.Product{}, repository.ErrProductNotFound
+	}
+
+	return product, nil
+
+}
+
+func (d *DefaultProductService) Create(product *models.Product) error {
+
+	if product.Name == "" {
+		return ErrNameIsNotEmpty
+	}
+
+	return d.productRepo.CreateProduct(product)
+
+}
+
+func (d *DefaultProductService) Edit(product *models.Product) error {
+	
+	if product.Id.String() == "" {
+		return  ErrIDIsNotValid
+	}
+	if product.Name == "" {
+		return ErrNameIsNotEmpty
+	}
+
+	err := d.productRepo.EditProduct(product)
+
+	if err != nil {
+		return  repository.ErrProductNotFound
+	}
+
+	return nil
+}
+
+func (d *DefaultProductService) Delete(id string) error {
+	int_id, _ := strconv.Atoi(id)
+	if int_id <= 0 {
+		return  ErrIDIsNotValid
+	}
+
+	err := d.productRepo.DeleteProduct(id)
+
+	if err != nil {
+		return repository.ErrProductNotFound
+	}
+
+	return nil
+
+}
