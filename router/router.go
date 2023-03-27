a46b7a9f78d442c8a87070ff3a8a444bd2f726a1
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-03-28 02:37:10 +0300

    router file updated

--- router/router.go
+++ router/router.go
@@ -1,19 +1,29 @@
 package router
 
 import (
+	"github.com/aabdullahgungor/product-api/controller"
+	"github.com/aabdullahgungor/product-api/repository"
+	"github.com/aabdullahgungor/product-api/service"
 	"github.com/gin-gonic/gin"
 )
 
+
+
 func ConfigRoutes(router *gin.Engine) *gin.Engine{
+
+	productRepo := repository.NewMongoDbProductRepository()
+	productService := service.NewDefaultProductService(productRepo)
+	productController := controller.NewProductController(productService)
+
 	main := router.Group("api/v1") 
 	{
 		products := main.Group("products")
 		{
-			products.GET("/", )
-			products.GET("/:id",)
-			products.POST("/", )
-			products.PUT("/", )
-			products.DELETE("/:id", )
+			products.GET("/",  productController.GetAllProducts)
+			products.GET("/:id", productController.GetProductById)
+			products.POST("/", productController.CreateProduct)
+			products.PUT("/", productController.EditProduct)
+			products.DELETE("/:id", productController.DeleteProduct)
 		}
 	}
 	return router
