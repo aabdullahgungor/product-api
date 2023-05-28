9ddf7060b4e2cf348ca0d8c7eccae6d18d7edd45
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-05-28 14:48:47 +0300

    router file updated for Swagger

--- router/router.go
+++ router/router.go
@@ -2,29 +2,40 @@ package router
 
 import (
 	"github.com/aabdullahgungor/product-api/controller"
+	"github.com/aabdullahgungor/product-api/docs"
 	"github.com/aabdullahgungor/product-api/repository"
 	"github.com/aabdullahgungor/product-api/service"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-
-
-func ConfigRoutes(router *gin.Engine) *gin.Engine{
+func ConfigRoutes(router *gin.Engine) *gin.Engine {
 
 	productRepo := repository.NewMongoDbProductRepository()
 	productService := service.NewDefaultProductService(productRepo)
 	productController := controller.NewProductController(productService)
 
-	main := router.Group("api/v1") 
+	main := router.Group("api/v1")
 	{
 		products := main.Group("products")
 		{
-			products.GET("/",  productController.GetAllProducts)
+			products.GET("/", productController.GetAllProducts)
 			products.GET("/:id", productController.GetProductById)
 			products.POST("/", productController.CreateProduct)
 			products.PUT("/", productController.EditProduct)
 			products.DELETE("/:id", productController.DeleteProduct)
 		}
+
+		// programmatically set swagger info
+		docs.SwaggerInfo.Title = "Swagger Example API"
+		docs.SwaggerInfo.Description = "This is a sample server ProductApi server."
+		docs.SwaggerInfo.Version = "1.0"
+		docs.SwaggerInfo.Host = ""
+		docs.SwaggerInfo.BasePath = "/api/v1"
+		docs.SwaggerInfo.Schemes = []string{"http", "https"}
+		// add swagger
+		main.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	}
 	return router
 }
